8810be02b9b1e66974f31f0ec4579a57b22055f1
carlo <carlo@CARLOS>
2025-01-17 15:54:42 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSounds2/Album.cs
@@ -0,0 +1,22 @@
+﻿class Album
+{
+    private List<Musica> musicaList = new List<Musica>();
+    public string Nome { get; set; }
+
+    public int duracaoTotal => musicaList.Sum(musica => musica.Duracao);
+
+    public void AddMusica(Musica musica)
+    {
+        musicaList.Add(musica);
+    }
+
+    public void ExibirMusicasAlbum()
+    {
+        Console.WriteLine($"Lista de musicas do album {Nome}:\n");
+        foreach (var musica in musicaList)
+        {
+            Console.WriteLine($"Musica: {musica.Nome}");
+        }
+        Console.WriteLine($"Este album possui {duracaoTotal} segundos");
+    }
+}
---
+++ ScreenSounds2/Program.cs
@@ -0,0 +1,37 @@
+﻿//// Objeto para a Musica
+//Musica musica1 =  new Musica();
+//Musica musica2 =  new Musica();
+
+//// Informaçoes da musica
+//musica1.Nome = "the emptiness machine";
+//musica1.Artista = "Linking Park";
+//musica1.Duracao = 255;
+//musica1.Disponivel = true;
+//Console.WriteLine(musica1.DescricaoResumida);
+
+//musica2.Nome = "Vertigo";
+//musica2.Artista = "U2";
+//musica2.Duracao = 267;
+//musica2.Disponivel = false;
+//Console.WriteLine(musica2.DescricaoResumida);
+
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+
+
+Album Queen = new Album();
+Queen.Nome = "A night at the Opera";
+
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my Life";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica();
+musica2.Nome =  "bohemian rhapsody";
+musica2.Duracao = 354;
+
+Queen.AddMusica(musica1);
+Queen.AddMusica(musica2);
+
+Queen.ExibirMusicasAlbum();
